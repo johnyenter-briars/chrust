68503e03b0e76899e2ce8ecd8b3de0df08ad5545
John Yenter-Briars <jyenterbriars@gmail.com>
2021-08-03 08:56:22 -0500

    add generic method for taking in input

--- src/game/chessgame.rs
+++ src/game/chessgame.rs
@@ -10,6 +10,7 @@ use crate::chessmove::action::{self, Action, HumanAction};
 
 use std::borrow::Borrow;
 use std::error::Error;
+use std::str::FromStr;
 
 pub struct ChessGame<'a> {
     pub human_player: HumanPlayer,
@@ -38,33 +39,32 @@ impl<'a> ChessGame<'a> {
             println!("Select your piece!");
 
             println!("x:");
-            let mut line = String::new();
-            let size = std::io::stdin().read_line(&mut line).unwrap();
-            let x: char = line.trim().parse()?;
+            let x: char = get_input::<char>()?;
 
             println!("y:");
-            let mut line = String::new();
-            let size = std::io::stdin().read_line(&mut line).unwrap();
-            let y: i32 = line.trim().parse()?;
+            let y: i32 = get_input::<i32>()?;
 
             println!("Your choice: x: {} y: {}", x, y);
             let current_position = Coordinate::new(x, y);
 
             //this is syntax is really cool
-            let coord_choices = match self.board.get_possible_moves(current_position, turn_num, &self.human_player) {
-                Ok(choices) => {
-                    if choices.len() < 1 {
-                        println!("That piece can't go anywhere!");
+            let coord_choices =
+                match self
+                    .board
+                    .get_possible_moves(current_position, turn_num, &self.human_player)
+                {
+                    Ok(choices) => {
+                        if choices.len() < 1 {
+                            println!("That piece can't go anywhere!");
+                            continue;
+                        }
+                        choices
+                    }
+                    Err(reason) => {
+                        println!("Not valid move : {:?}", reason);
                         continue;
                     }
-                    choices
-                },
-                Err(reason) => {
-                    println!("Not valid move : {:?}", reason);
-                    continue;
-                }
-            };
-
+                };
 
             println!("{:?}", coord_choices);
             for (index, c) in coord_choices.iter().enumerate() {
@@ -89,4 +89,14 @@ impl<'a> ChessGame<'a> {
 
         Ok(&self.human_player.name)
     }
+}
+
+//this is a mess and i dont fully understand it - i just did what the compiler told me to
+fn get_input<I: std::str::FromStr>() -> 
+Result<I, Box<dyn Error>> where <I as FromStr>::Err: 'static + Error 
+{
+    let mut line = String::new();
+    let _ = std::io::stdin().read_line(&mut line).unwrap();
+    let y: I = line.trim().parse()?;
+    Ok(y)
 }
