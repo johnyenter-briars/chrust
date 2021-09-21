06e865c970df081c56311be788c29f0dcb7f50c8
John Yenter-Briars <jyenterbriars@gmail.com>
2021-09-21 17:21:37 -0500

    update server to use relative path

---
+++ build_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+VERSION=0.1.0
+TARGET_DIR=./release/linux/chrust-$VERSION/
+BUILD_TARGET=x86_64-unknown-linux-gnu
+
+cargo build --target $BUILD_TARGET --release
+
+mkdir -p $TARGET_DIR && cp ./target/$BUILD_TARGET/release/chrust $TARGET_DIR
+mkdir -p $TARGET_DIR && cp -r ./static ./boards $TARGET_DIR
